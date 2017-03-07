8b49988bc5b21eafca3c3191e7d9de390ca97b57
pjsinco <psinco@gmail.com>
2017-03-07 09:46:14 -0600

    Get copy-HTML button to copy HTML

--- src/components/jaoa/article.vue
+++ src/components/jaoa/article.vue
@@ -15,11 +15,55 @@
 <script>
 
   import { jaoa } from './../../app-constants'
+  import Clipboard from './../../../node_modules/clipboard/dist/clipboard.min.js'
 
   export default {
 
     methods: {
+      onMouseLeave: function() {
+console.log('mouseleave');
+        //$('#copyHtml').removeClass('tooltipped tooltipped-w');
+      },
 
+      collapseSelection: function(elementId) {
+
+        const elem = document.getElementById(elementId)
+
+        if (window.getSelection()) {
+            window.getSelection().collapse(elem, 0)
+        }
+      },
+
+      cloneRenderedVersion: function() {
+        const htmlVersion = document.getElementById('rendered')
+        document.getElementById('clone').innerText = htmlVersion.innerHTML
+      },
+
+      copyHtml: function() {
+
+        this.collapseSelection('clone');
+        this.cloneRenderedVersion();
+
+        const clipboard = new Clipboard('#copyHtml', {
+          target: function(trigger) {
+            return document.getElementById('clone');
+          }
+        });
+
+        clipboard.on('success', function(evt) {
+          console.log('Copied!');
+//          $('#highlight').addClass('animated jello');
+//          $('#highlight').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+//            $(this).removeClass('animated jello');
+//          });
+//          $('#copyHtml').addClass('tooltipped tooltipped-w');
+          clipboard.destroy();
+        });
+      },
+
+      copyTextVersion: function() {
+
+      },
     },
 
     mounted: function() {
