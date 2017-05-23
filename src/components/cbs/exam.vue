1bdb10858c116a198eac3db1791811cf0900db0d
pjsinco <psinco@gmail.com>
2017-05-23 14:13:15 -0500

    Wire up adding, removing exams

---
+++ src/components/cbs/exam.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+
+</template>
+
+<script>
+
+  export default {
+
+    name: 'cbs-exam',
+
+    data: function() {
+      
+      return {
+        location: '',
+        date: '',
+        firstDeadline: '',
+        secondDeadline: '',
+      };
+    }
+
+  };
+
+</script>
+
+<style lang="scss">
+
+</style>
--- src/components/cbs/expiration.vue
+++ src/components/cbs/expiration.vue
@@ -15,6 +15,7 @@
 <script>
   import { cbs } from './../../app-constants'
   import mixins from './../../mixins'
+  import cbsExam from './exam.vue'
 
   export default {
 
@@ -23,6 +24,32 @@
     mixins: [mixins],
 
     methods: {
+
+      removeExam: function(evt) {
+        this.exams.splice(evt.currentTarget.dataset.exam, 1);
+      },
+
+      addExam: function(evt) {
+        this.showExamForm = !this.showExamForm;
+
+        const exam = {
+          location: '',
+          date: '',
+          firstDeadline: '',
+          finalDeadline: '',
+        }
+        this.exams.push(exam);
+      },
+
+      copy: function() {
+        this.copyHtml()
+      },
+
+      copyTextVersion: function(evt) {
+        const text = 'Dear %%name%%';
+        this.copyText(text);
+      },
+
     },
   
     data: function() {
@@ -30,6 +57,8 @@
         boardFriendly: cbs.boards['Dermatology'].friendly,
         board: cbs.boards['Dermatology'],
         cbs,
+        examCount: 0,
+        exams: [],
       }
     },
 
@@ -40,6 +69,9 @@
     },
 
     computed: {
+      showExamForm: function() {
+        return this.exams.length > 0;
+      },
       sortedBoards: function() {
         return Object.keys(cbs.boards).sort();
       } 
