4322f5f4cad6a065f4727cf2b3c10b337c50cb04
pjsinco <psinco@gmail.com>
2017-05-24 08:59:19 -0500

    Set up simple routing

--- src/Jaoa.vue
+++ src/Jaoa.vue
@@ -29,7 +29,7 @@
 
   export default {
 
-    name: 'app',
+    name: 'jaoa',
 
     components: {
       'rupper-header': rupperHeader,
--- src/main.js
+++ src/main.js
@@ -1,10 +1,29 @@
 import Vue from 'vue'
-import App from './App.vue'
+import Jaoa from './Jaoa.vue'
+import Cbs from './Cbs.vue'
+
+const routes = {
+  '/jaoa': Jaoa,
+  '/cbs': Cbs,
+};
 
 new Vue({
+
   el: '#app',
-  render: h => h(App)
+  
+  data: {
+    currentRoute: window.location.pathname
+  },
+
+  mounted: function() {
+    console.log(routes[this.currentRoute]);
+  },
+
+  computed: {
+    ViewComponent() {
+      return routes[this.currentRoute]
+    }
+  },
+
+  render(h) { return h(this.ViewComponent) }
 })
-
-console.log('hiyy');
-
