fc902ece3ee636c08de1cd5be973be8285a3882c
pjsinco <psinco@gmail.com>
2017-05-26 13:01:49 -0500

    Add quote component..
    No longer break components into render and form child components

--- src/components/the-do/date.vue
+++ src/components/the-do/date.vue
@@ -2,10 +2,10 @@
   div#workspace.clearfix.workspace
     div.form-container
       div#form.form
-        date-form(v-bind:initial-date="date" v-bind:copy-text-version="copyTextVersion" v-bind:copy="copy" v-on:datechange="handleDateChange")
+        include ./../../views/the-do/forms/date
     div.rendered-container
       div#rendered
-        date-render(v-bind:date="date")
+        include ./../../views/the-do/renders/date
     div#clone(style="position: absolute; top: -5000px; left: -5000px")
 
 </template>
@@ -13,25 +13,14 @@
 <script>
   import mixins from './../../mixins';
   import moment from 'moment';
-  import dateForm from './forms/date-form.vue'
-  import dateRender from './renders/date-render.vue'
-  
 
   export default {
-
-    components: {
-      'date-form': dateForm,
-      'date-render': dateRender,
-    },
 
     name: 'the-do-date',
 
     mixins: [mixins],
 
     methods: {
-      handleDateChange: function(evt) {
-        this.date = evt.target.value;
-      },
 
       copy: function() {
         this.copyHtml();
@@ -49,7 +38,6 @@
     },
 
     data: function() {
-
       return {
         date: moment().format('MMMM D, YYYY'),
       };
---
+++ src/components/the-do/quote.vue
@@ -0,0 +1,53 @@
+<template lang="pug">
+  div#workspace.clearfix.workspace
+    div.form-container
+      div#form.form
+        include ./../../views/the-do/forms/quote
+
+    div.rendered-container
+      div#rendered
+        include ./../../views/the-do/renders/quote
+
+    div#clone(style="position: absolute; top: -5000px; left: -5000px")
+</template>
+
+<script>
+
+  import mixins from './../../mixins';
+
+  export default {
+
+    name: 'the-do-quote',
+
+    mixins: [mixins],
+
+    methods: {
+      copy: function() {
+        this.copyHtml();
+      },
+
+      copyTextVersion: function() {
+
+        const text = [
+            '\n',
+            'Q U O T E D',
+            '\n',
+            this.quote,
+            '--' + this.quoter,
+            this.link,
+        ].join('\n');
+
+        return this.copyText(text);
+      },
+    },
+
+    data: function() {
+      return {
+        quote: 'Lorem quote',
+        quoter: 'Lorem quoter',
+        link: '',
+      };
+    },
+  }
+
+</script>
